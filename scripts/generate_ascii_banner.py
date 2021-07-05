01d2d7c0f82d75588c272bf54bf935a5cb98465d
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-07-05 14:54:47 -0400

    Modify banner text to better match the font used in the Python logo.

--- scripts/generate_ascii_banner.py
+++ scripts/generate_ascii_banner.py
@@ -16,21 +16,22 @@ version online at:
 ASCII FONT:
 
 The "python powered" text was generated with FIGlet using the Jazmine
-font (jazmine.flf).
+font (jazmine.flf). The banner text was then modified to better match
+the letter shapes used in the Python logo.
 """
 
 ascii_art = """
-           .....                                   o  8
+           .....                                      8
          d88888888.                                8  8
-        8  88888888                .oPYo. o    o  o8P 8oPYo. .oPYo. odYo.
-        88888888888                8    8 8    8   8  8    8 8    8 8' `8
-      .......888888                8    8 8    8   8  8    8 8    8 8   8
- ,88888888888888888 ######.        8YooP' `YooP8   8  8    8 `YooP' 8   8
+        8  88888888                .oPYo. o    o  o8P 8oPYo. .oPYo. odPYo.
+        88888888888                8    8 8    8   8  8    8 8    8 8'  `8
+      .......888888                8    8 8    8   8  8    8 8    8 8    8
+ ,88888888888888888 ######.        8YooP' `YooP8   'Y 8    8 `YooP' 8    8
 d888888888888888888 #######;       8           8
-88888888888888888' ,########       8        ooP'                              8
+88888888888888888' ,########       8         oP'                              8
 88888888' ,#################                                                  8
-V8888888 ##################" .oPYo. .oPYo. o   o   o .oPYo. oPYo. .oPYo. .oPYo8
- ^888888 #################'  8    8 8    8 Y. .P. .P 8oooo8 8  `' 8oooo8 8    8
+V8888888 ##################" .oPYo. .oPYo. o   o   o .oPYo. oPY^' .oPYo. .oPYo8
+ ^888888 #################'  8    8 8    8 Y. .P. .P 8oooo8 8     8oooo8 8    8
          ######'''''''       8    8 8    8 `b.d'b.d' 8.     8     8.     8    8
          ###########         8YooP' `YooP'  `Y' `Y'  `Yooo' 8     `Yooo' `YooP'
          ########  #         8
