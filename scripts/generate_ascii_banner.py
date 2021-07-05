6f78a0ffff6e2b3685bc4b6aed76f627ac3edb04
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-07-05 14:18:43 -0400

    Add script for generating ASCII art banner.

---
+++ scripts/generate_ascii_banner.py
@@ -0,0 +1,34 @@
+"""ASCII Art Generation Script
+
+
+PYTHON LOGO:
+
+The original Python logo was designed by Tim Parkin and is trademarked
+by the Python Software Foundation.
+
+An ASCII art version of the logo was designed by Matthew Barber and is
+licensed under CC-BY-4.0. Matthew Barber's ASCII version was used as a
+reference for the logo I created here. You can find Matthew Barber's
+version online at:
+
+    https://ascii.matthewbarber.io/art/python/
+"""
+
+ascii_art = """
+           .....
+         d88888888.
+        8  88888888
+        88888888888
+      .......888888
+ ,88888888888888888 888888.
+d888888888888888888 8888888b
+88888888888888888' ,88888888
+88888888' ,88888888888888888
+V8888888 888888888888888888"
+ ^888888 88888888888888888'
+         888888'''''''
+         88888888888
+         88888888  8
+         ^88888888P
+            '''''
+"""
