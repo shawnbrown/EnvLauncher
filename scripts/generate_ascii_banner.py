b1910d471e78088170bb293ff20d2e014acb4b57
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-07-05 14:38:55 -0400

    Add ASCII banner text to mimic the "Python Powered" logo (wide format).

--- scripts/generate_ascii_banner.py
+++ scripts/generate_ascii_banner.py
@@ -12,23 +12,28 @@ reference for the logo I created here. You can find Matthew Barber's
 version online at:
 
     https://ascii.matthewbarber.io/art/python/
+
+ASCII FONT:
+
+The "python powered" text was generated with FIGlet using the Jazmine
+font (jazmine.flf).
 """
 
 ascii_art = """
-           .....
-         d88888888.
-        8  88888888
-        88888888888
-      .......888888
- ,88888888888888888 ######.
-d888888888888888888 #######;
-88888888888888888' ,########
-88888888' ,#################
-V8888888 ##################"
- ^888888 #################'
-         ######'''''''
-         ###########
-         ########  #
-         '########"
+           .....                                   o  8
+         d88888888.                                8  8
+        8  88888888                .oPYo. o    o  o8P 8oPYo. .oPYo. odYo.
+        88888888888                8    8 8    8   8  8    8 8    8 8' `8
+      .......888888                8    8 8    8   8  8    8 8    8 8   8
+ ,88888888888888888 ######.        8YooP' `YooP8   8  8    8 `YooP' 8   8
+d888888888888888888 #######;       8           8
+88888888888888888' ,########       8        ooP'                              8
+88888888' ,#################                                                  8
+V8888888 ##################" .oPYo. .oPYo. o   o   o .oPYo. oPYo. .oPYo. .oPYo8
+ ^888888 #################'  8    8 8    8 Y. .P. .P 8oooo8 8  `' 8oooo8 8    8
+         ######'''''''       8    8 8    8 `b.d'b.d' 8.     8     8.     8    8
+         ###########         8YooP' `YooP'  `Y' `Y'  `Yooo' 8     `Yooo' `YooP'
+         ########  #         8
+         '########"          8
             '''''
 """
