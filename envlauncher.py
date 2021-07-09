922adc1bf3bddc0175748de8946df2055fa9c32e
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-07-08 23:01:48 -0400

    Add GNU GPL license notices to source files.

--- envlauncher.py
+++ envlauncher.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# Copyright (C) 2021 Shawn Brown.
+#
+# This file is part of EnvLauncher.
+#
+# EnvLauncher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EnvLauncher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EnvLauncher.  If not, see <https://www.gnu.org/licenses/>.
+
 import subprocess
 import tempfile
 
