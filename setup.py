c016d7fe0a2f6be0d294fe966c5fdd2edf72a7cd
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-07-16 21:32:47 -0400

    Add "Linux" trove classifier and remove "OS Independent".
    Perhaps future versions of EnvLauncher will be OS independent
    but for now it's Linux-only.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             'Development Status :: 2 - Pre-Alpha',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Operating System :: OS Independent',
+            'Operating System :: POSIX :: Linux',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
